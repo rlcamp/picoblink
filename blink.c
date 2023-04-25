574ec45aad3f45cdf6aed5f17f3f29f54a3baf80
Richard Campbell <rlcamp@users.noreply.github.com>
2023-04-24 23:32:13 -0700

    use cmsis systick for timer, and underclock to 48 MHz

--- blink.c
+++ blink.c
@@ -1,17 +1,37 @@
 #include "pico/stdlib.h"
+#include "RP2040.h"
+
+static unsigned long ticks = 0;
+
+void SysTick_Handler(void) {
+    ticks++;
+}
+
+static int one_interval_has_elapsed(unsigned long interval_ticks) {
+    static unsigned long prev;
+    if (ticks - prev < interval_ticks) {
+        __WFI();
+        return 0;
+    }
+    prev += interval_ticks;
+    return 1;
+}
 
 int main() {
     /* 25 for pico, 13 for adafruit feather rp2040 */
-    const unsigned pin = 13;
+    static const unsigned pin = PICO_DEFAULT_LED_PIN;
+
+    set_sys_clock_48mhz();
+    SysTick_Config(12000000UL);
 
     gpio_init(pin);
     gpio_set_dir(pin, GPIO_OUT);
 
     while (true) {
         gpio_put(pin, 1);
-        sleep_ms(500);
+        while (!one_interval_has_elapsed(2)) continue;
 
         gpio_put(pin, 0);
-        sleep_ms(250);
+        while (!one_interval_has_elapsed(1)) continue;
     }
 }
