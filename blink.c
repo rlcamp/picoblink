aee7596dfebcdba6355a1a5cd7df5f9bef7eb9d5
Richard Campbell <rlcamp@users.noreply.github.com>
2025-06-10 18:24:42 -0700

    use sevonpend instead of isr

--- blink.c
+++ blink.c
@@ -12,21 +12,6 @@
 
 #define ALARM_NUM 0
 
-/* not volatile, because we don't want to obligate ISRs to make volatile accesses to it,
- and we can still enforce volatile access to it within main loop */
-static unsigned alarms = 0;
-
-static void alarm_irq(void) {
-    /* acknowledge interrupt */
-    hw_clear_bits(&timer_hw->intr, 1u << ALARM_NUM);
-
-    /* nonvolatile increment of number of alarms that have happened */
-    alarms++;
-
-    /* increment and rearm */
-    timer_hw->alarm[ALARM_NUM] += LED_DELAY_US;
-}
-
 void yield(void) {
     /* we could do context switching here for cooperative multitasking if we wanted */
     __dsb();
@@ -53,6 +38,9 @@ void run_from_xosc(void) {
 }
 
 int main() {
+    /* enable sevonpend so that we don't need a nearly empty isr */
+    scb_hw->scr |= M33_SCR_SEVONPEND_BITS;
+
     /* this is not a cpu intensive application, just run at 12 MHz */
     run_from_xosc();
 
@@ -85,22 +73,28 @@ int main() {
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
-    /* enable interrupt for alarm */
+    /* enable interrupt for alarm in peripheral, but not in nvic */
     hw_set_bits(&timer_hw->inte, 1u << ALARM_NUM);
-    irq_set_exclusive_handler(hardware_alarm_get_irq_num(ALARM_NUM), alarm_irq);
-    irq_set_enabled(hardware_alarm_get_irq_num(ALARM_NUM), true);
+    irq_set_enabled(hardware_alarm_get_irq_num(ALARM_NUM), false);
 
     /* first tick will be one interval from now */
     timer_hw->alarm[ALARM_NUM] = timer_hw->timerawl + LED_DELAY_US;
 
-    unsigned alarms_acknowledged = 0;
-    while (1) {
-        if (!(alarms_acknowledged % 4))
+    for (unsigned alarms = 0;; alarms++) {
+        /* repeatedly sleep, until timer interrupt becomes pending */
+        while (!(timer_hw->intr & (1U << ALARM_NUM)))
+            yield();
+
+        /* acknowledge and clear interrupt in timer and nvic */
+        hw_clear_bits(&timer_hw->intr, 1U << ALARM_NUM);
+        irq_clear(hardware_alarm_get_irq_num(ALARM_NUM));
+
+        /* increment and rearm */
+        timer_hw->alarm[ALARM_NUM] += LED_DELAY_US;
+
+        if (!(alarms % 4))
             gpio_put(PICO_DEFAULT_LED_PIN, 1);
         else
             gpio_put(PICO_DEFAULT_LED_PIN, 0);
-
-        while (*(volatile unsigned *)&alarms == alarms_acknowledged) yield();
-        alarms_acknowledged++;
     }
 }
